ce405d006a7dfc8fb301e4dfcf320974a721b18a
SandeepKumarSingh0908 <san.singh39@gmail.com>
2022-07-26 14:08:33 -0700

    Hibernate Configuration and basic object saving code

--- HibernatePractise/src/main/java/com/sandeep/hibernate/App.java
+++ HibernatePractise/src/main/java/com/sandeep/hibernate/App.java
@@ -1,5 +1,12 @@
 package com.sandeep.hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.sandeep.hibernate.entity.Student;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       System.out.println("Project Started");
+       
+       SessionFactory factory = new Configuration().configure().buildSessionFactory();
+       
+       Student student = new Student(1,"Sandeep","Ranchi");
+       
+       System.out.println(student);
+       Session session = factory.openSession();
+       
+       Transaction tx = session.beginTransaction();
+       
+       session.save(student);
+       
+       tx.commit();
+       
+       session.close();
+       
+       
+       
     }
 }
